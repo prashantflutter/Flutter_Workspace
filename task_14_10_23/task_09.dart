94bc0da7b16c99af99a5ec27e01d38c768b7feca
PRASHANT VADHER <147711836+prashantflutter@users.noreply.github.com>
2023-10-16 14:25:15 +0530

    Update task_09.dart

--- task_14_10_23/task_09.dart
+++ task_14_10_23/task_09.dart
@@ -2,8 +2,8 @@
 
 
 void main(){
-  int year = 2023;
-  if(year % 4 == 0 && year % 400 == 0 ){
+  int year = 2004;
+  if(((year%400==0)||(year%100!=0)) && year % 4 == 0 ){
     print(" $year is leap year.");
   }else{
     print("$year is not leap year.");
