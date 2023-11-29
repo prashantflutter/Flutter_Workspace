eb8dc2c796b8cea08fe0b8cf1db858bb9257f11f
PRASHANT VADHER <147711836+prashantflutter@users.noreply.github.com>
2023-11-29 14:12:35 +0530

    Update task_25_23.dart

--- Flutter Tasks/task_25_23.dart
+++ Flutter Tasks/task_25_23.dart
@@ -66,9 +66,14 @@ class _LoginPageState extends State<LoginPage> {
                   email.text = value;
                 },
                 onSaved: (value){
+
                   email.text = value.toString();
                 },
                 validator: (value){
+
+                  String pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                  RegExp regex = new RegExp(pattern);
+                  return (!regex.hasMatch(value!)) ? "Enter Valid Email id" : null;
                   if(value.toString().isEmpty){
                     return "Enter Email id";
                   }
