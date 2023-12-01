80621ac3755f9d96530ed166794d52f18b8edc2b
PRASHANT VADHER <147711836+prashantflutter@users.noreply.github.com>
2023-12-01 12:37:44 +0530

    Update task_50.dart

--- Module 3/task_50.dart
+++ Module 3/task_50.dart
@@ -1,161 +1,128 @@
-// Create an application to take input number from user and print its
-// reverse number in TextField
+// 51. Create an application to input 2 numbers from user and all numbers
+// between those 2 numbers in next activity
 
 import 'package:flutter/material.dart';
 
-class ReversePrintPage extends StatefulWidget {
-  const ReversePrintPage({Key? key}) : super(key: key);
-
+class NumberRangeScreen extends StatefulWidget {
   @override
-  State<ReversePrintPage> createState() => _ReversePrintPageState();
+  _NumberRangeScreenState createState() => _NumberRangeScreenState();
 }
 
-class _ReversePrintPageState extends State<ReversePrintPage> {
+class _NumberRangeScreenState extends State<NumberRangeScreen> {
+  TextEditingController _startController = TextEditingController();
+  TextEditingController _endController = TextEditingController();
+  List<int> numbers = [];
 
-  TextEditingController numberController = TextEditingController();
-  TextEditingController resultController = TextEditingController();
+  void printNumbersBetween() {
+    int start = int.tryParse(_startController.text) ?? 0;
+    int end = int.tryParse(_endController.text) ?? 0;
 
-  int reversedNumber = 0;
-  String? gender; //no radio button will be selected on initial
+    
+    if (start <= end) {
+      int last = end - 1;
+      setState(() {
+        numbers = List.generate(last - start, (index) => start + index +1);
+      });
+    }
+    else if (start > end) {
+      int last = end+1;
+      setState(() {
+        numbers = List.generate(start - last, (index) => start - index - 1);
 
+      });
+    }
+      else {
+      setState(() {
+        numbers = [];
+      });
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Invalid Range'),
+            content: Text('The start value should be less than the end value.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 50,),
-            Text("Enter the Value to Reverse Printing.",textAlign:TextAlign.center,style: TextStyle(fontSize: 30,color: Colors.blueAccent,fontWeight: FontWeight.bold),),
-            SizedBox(height: 70,),
-            TextFormField(
-              controller: numberController,
-              style: TextStyle(fontSize: 18,color: Colors.blueAccent.shade700,fontWeight: FontWeight.normal),
-              decoration: InputDecoration(
-                label: Text("Reverse Printing"),
-                prefixIcon: Icon(Icons.autorenew_rounded,color: Colors.white,size: 25,),
-                hintText: "54321",
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent.shade700,width: 5),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                hintStyle: TextStyle(fontSize: 18,color: Colors.white,fontWeight: FontWeight.normal),
+      appBar: AppBar(
+        title: Text('Number Range Printer'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Two Numbers between Value Printing',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green,fontWeight:FontWeight.w600 ,fontSize: 28.0),
               ),
-            ),
-            SizedBox(height: 30,),
-            SizedBox(
-              width: 220,
-              height: 60,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blueAccent
+              SizedBox(height: 24),
+              TextField(
+                controller: _startController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Enter start number',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green,width: 2),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
                 ),
-                onPressed: (){
-                  if(numberController.text.isEmpty){
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        backgroundColor: Colors.redAccent,
-                        behavior: SnackBarBehavior.floating,
-                        content: Text("Please Enter the Value...",style: TextStyle(color: Colors.white,fontSize: 18),),),
-                    );
-                  }else{
-                    print(numberController.text);
-                    reversePrinting(numberController.text);
-                    resultController.text = reversedNumber.toString();
-                    setState(() {});
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        backgroundColor: Colors.green,
-                        behavior: SnackBarBehavior.floating,
-                        content: Text(gender.toString(),style: TextStyle(color: Colors.white,fontSize: 18),),),
-                    );
-                  }
-
-                },
-                child: Text("Submit",style: TextStyle(color: Colors.white,fontSize: 18),),
               ),
-            ),
-            SizedBox(height: 30,),
-            TextFormField(
-              readOnly: true,
-              controller: resultController,
-              style: TextStyle(fontSize: 18,color: Colors.blueAccent.shade700,fontWeight: FontWeight.normal),
-              decoration: InputDecoration(
-                label: Text("Reverse Printing"),
-                prefixIcon: Icon(Icons.autorenew_rounded,color: Colors.white,size: 25,),
-                hintText: "Reverse Value ",
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(width:5,color: Colors.blueAccent.shade700,),
-                  borderRadius: BorderRadius.circular(12),
+              SizedBox(height: 16),
+              TextField(
+                controller: _endController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Enter end number',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green,width: 2),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
                 ),
-                hintStyle: TextStyle(fontSize: 18,color: Colors.white,fontWeight: FontWeight.normal),
               ),
-            ),
-            SizedBox(height: 20,),
-            Column(
-              children: [
-
-                RadioListTile(
-                  title: Text("Male"),
-                  value: "male",
-                  groupValue: gender,
-                  activeColor: Colors.blueAccent.shade700,
-                  onChanged: (value){
-                    setState(() {
-                      gender = value.toString();
-                    });
-                  },
+              SizedBox(height: 24),
+              SizedBox(
+                width: 150,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: printNumbersBetween,
+                  child: Text('Print Numbers'),
                 ),
-
-                RadioListTile(
-                  title: Text("Female"),
-                  value: "female",
-                  groupValue: gender,
-                  activeColor: Colors.blueAccent.shade700,
-                  onChanged: (value){
-                    setState(() {
-                      gender = value.toString();
-                    });
-                  },
-                ),
-
-                RadioListTile(
-                  title: Text("Other"),
-                  value: "other",
-                  groupValue: gender,
-                  activeColor: Colors.blueAccent.shade700,
-                  onChanged: (value){
-                    setState(() {
-                      gender = value.toString();
-                    });
-                  },
-                )
-              ],
-            )
-          ],
+              ),
+              SizedBox(height: 24),
+              Text(
+                'Numbers between: ${numbers.join(", ")}',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ],
+          ),
         ),
       ),
     );
-
-
   }
 
-  Future<int>reversePrinting(String value)async{
-    String numberString = value.toString(); // Convert the number to a string
-    int i = numberString.length - 1;
-    reversedNumber = 0;
-    while (i >= 0) {
-      int digit = int.parse(numberString[i]); // Get the digit at the current position
-      reversedNumber = reversedNumber * 10 + digit; // Build the reversed number
-      i--;
-    }
-
-    return reversedNumber;
-
+  @override
+  void dispose() {
+    _startController.dispose();
+    _endController.dispose();
+    super.dispose();
   }
 }
