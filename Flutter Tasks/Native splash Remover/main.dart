e87470655184d34b8b7954254cda711f53cbb332
PRASHANT VADHER <147711836+prashantflutter@users.noreply.github.com>
2023-12-27 10:47:42 +0530

    Add files via upload

---
+++ Flutter Tasks/Native splash Remover/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:shidhapura/screes/home_screen.dart';
+import 'package:shidhapura/screes/login_screen.dart';
+import 'screes/new_enquire_screen.dart';
+import 'screes/splash_screen.dart';
+
+void main() {
+  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove();
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      //   useMaterial3: true,
+      // ),
+      home: SplashScreenPage(),
+    );
+  }
+}
+
