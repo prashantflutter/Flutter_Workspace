a8d631eaa3023bc2288f3fa78bba360a0f0d0306
PRASHANT VADHER <147711836+prashantflutter@users.noreply.github.com>
2024-01-11 11:02:07 +0530

    Add files via upload

---
+++ module4/task_78/user_model.dart
@@ -0,0 +1,8 @@
+class UserModel{
+  late String fName;
+  late String lName;
+  late String courseName;
+  late String companyName;
+
+  UserModel(this.fName, this.lName, this.courseName, this.companyName);
+}
